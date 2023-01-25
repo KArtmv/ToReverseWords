e80e364467ae700db6b08e0d44884bf687064398
kiril <kiril@192.168.0.220>
2023-01-25 20:44:31 +0100

    changed method for to reverse many words

--- ToReverseWords/src/main/java/ua/com/foxminded/java/spring/ToReverseWords.java
+++ ToReverseWords/src/main/java/ua/com/foxminded/java/spring/ToReverseWords.java
@@ -2,9 +2,18 @@ package ua.com.foxminded.java.spring;
 
 public class ToReverseWords {
 
-	public static String reverseWord (String inputWorld) {
-		String outputWords = new StringBuilder(inputWorld).reverse().toString();
-		return outputWords;
+	public static String reverseWord(String inputWords) {
+		int i = 0;
+		String arrayWords[] = inputWords.split(" ");
+		String outputWords[] = new String[arrayWords.length];
+
+		for (String words : arrayWords) {
+			String reverse = new StringBuilder(words).reverse().toString();
+			outputWords[i] = reverse;
+			i++;
 		}
-	
+
+		return String.join(" ", outputWords);
+	}
+
 }
